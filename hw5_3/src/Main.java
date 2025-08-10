b0c1bf644c136ebed3409bf545439af6faf1e627
ksa237 <ksa237@yandex.ru>
2025-08-10 17:08:23 +1000

    hw5_3 attemp 1

--- hw5_3/src/Main.java
+++ hw5_3/src/Main.java
@@ -1,5 +1,75 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+
+        int[][] teams = {
+                {45, 31, 24, 22, 20, 17, 14, 13, 12, 10},
+                {31, 18, 15, 12, 10, 8, 6, 4, 2, 1},
+                {51, 30, 10, 9, 8, 7, 6, 5, 2, 1}
+        };
+
+        int[] nationalTeam = mergeAll(teams);
+        System.out.println(Arrays.toString(nationalTeam)); // [51, 45, 31, 31, 30, 24, 22, 20, 18, 17]
+
+    }
+
+
+    /**
+     * Метод для слияния всех команд в одну национальную
+     */
+    public static int[] mergeAll(int[][] teams) {
+        ArrayList<int[]> allTeams = new ArrayList<>();
+
+        int[] answer = new int[10];
+
+        for (int[] arNext : teams) { //artInt - одномерный массив, топ-команды одного региона
+            //System.out.println("#1 " + Arrays.toString(arNext));
+            //System.out.println("#2 " + Arrays.toString(answer));
+            int[] preAnswer = merge(answer, arNext);
+            answer = preAnswer;
+            //System.out.println("merge: " + Arrays.toString(answer));
+            //System.out.println();
+        }
+        return answer;
+    }
+
+    /**
+     * Метод для слияния двух команд в одну
+     */
+    public static int[] merge(int[] teamA, int[] teamB) {
+        int[] c = new int[teamA.length];
+        int ia = 0;
+        int ib = 0;
+        int ic = 0;
+
+        while (ia < teamA.length || ib < teamB.length) {
+
+            if (ia < teamA.length && ib < teamB.length) {
+
+                if (teamA[ia] >= teamB[ib]) {
+                    c[ic] = teamA[ia];
+                    ia += 1;
+                } else {
+                    c[ic] = teamB[ib];
+                    ib += 1;
+                }
+
+            } else if (ib == teamB.length) {
+                c[ic] = teamA[ia];
+                ia = +1;
+            } else if (ia == teamA.length) {
+                c[ic] = teamB[ib];
+                ib += 1;
+            }
+
+            if (ic == 9) {
+                break;
+            }
+            ic += 1;
+        }
+        return c;
     }
 }
